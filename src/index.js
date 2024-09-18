9ce8c0a42274d7cbaf8f8921865b318ab5a7c9fa
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-09-18 17:41:17 +0500

    fixes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const startServer = require("./config/server.config");
-const socketRoutes = require("./routes/socket.routes");
+// const socketRoutes = require("./routes/socket.routes");
 
 // middleware
 app.use(express.json());
